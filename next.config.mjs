baf0be00a5f3d65b4d1031040e88e4d4db415779
devgiljong <calf-love@naver.com>
2024-05-19 23:50:06 +0900

    :construction: Utils add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
