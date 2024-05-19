23863b7ce779b4a92ae1970cbbad288a38005bc7
devgiljong <calf-love@naver.com>
2024-05-20 00:16:26 +0900

    :construction: Provider

--- src/app/provider/providers.tsx
+++ src/app/provider/providers.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { builder } from '~/src/shared/hooks/builder';
-import { QueryProvider } from './query-provider';
+import QueryProvider from './query-provider';
 import GlobalStateProvider from './global-state-provider';
 
 const Providers = builder.tree([builder.provider(QueryProvider, {}), builder.provider(GlobalStateProvider, {})]);
--- src/app/provider/query-provider.tsx
+++ src/app/provider/query-provider.tsx
@@ -1,36 +1,24 @@
 'use client';
 
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type * as React from 'react';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (typeof window === 'undefined') {
-    return makeQueryClient();
-  }
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
-  return browserQueryClient;
-}
-
-export function QueryProvider(props: { children: React.ReactNode }) {
-  const queryClient = getQueryClient();
+export default function QueryProvider({ children }: React.PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryStreamedHydration>{props.children}</ReactQueryStreamedHydration>
+      {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
