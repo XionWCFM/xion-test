b21de71d8fd2ae0beddd55675b230b1f83fd3b92
devgiljong <calf-love@naver.com>
2024-05-19 23:05:11 +0900

    :construction: Msw setting

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,19 @@
+import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 import createFetchMock from 'vitest-fetch-mock';
-import '@testing-library/jest-dom';
+import { mockServer as server } from './src/mocks/node';
 
 const fetchMocker = createFetchMock(vi);
 fetchMocker.enableMocks();
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
