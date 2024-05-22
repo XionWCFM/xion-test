7144a4be51ec67b452c0593c230e190018632c34
devgiljong <calf-love@naver.com>
2024-05-22 16:37:58 +0900

    :construction: Repository

--- src/entities/example/api/example.api.ts
+++ src/entities/example/api/example.api.ts
@@ -1,4 +1,24 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import type { CartItem } from '../model/example.model';
+
+export const createExampleRepository = () => {
+  return {
+    readAll: async () => {},
+    add: async (body: CartItem) => {
+      const response = await fetch('/api/hello', {
+        method: 'POST',
+        body: JSON.stringify(body),
+      });
+      const result = await response.json();
+      return result;
+    },
+    delete: async (id: CartItem['id']) => {
+      await fetch(`/api/hello/${id}`, {
+        method: 'DELETE',
+      });
+    },
+  };
+};
 
 export const useExampleQuery = () => {
   return useSuspenseQuery({
