a75880181f4fb807a6ff0ac8c3fadc2d1c34ea6e
devgiljong <calf-love@naver.com>
2024-05-25 15:52:49 +0900

    :construction: Link props 확장

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Link, type LinkType } from '~/src/shared/adapters/link';
+
 export default function Home() {
-  return <div>hello world</div>;
+  return <HomePage Link={Link} />;
 }
+
+const HomePage = ({ Link }: { Link: LinkType }) => {
+  return (
+    <div>
+      <Link href={'/gonext'}>go next</Link>
+    </div>
+  );
+};
--- src/shared/adapters/link/index.tsx
+++ src/shared/adapters/link/index.tsx
@@ -1,5 +1,5 @@
 import NextLink from 'next/link';
-import { type Ref, forwardRef, type ReactNode } from 'react';
+import { type Ref, forwardRef, type ReactNode, type ComponentPropsWithoutRef, type ComponentType } from 'react';
 
 type LinkProps = {
   href: string;
@@ -7,12 +7,14 @@ type LinkProps = {
   scroll?: boolean;
   prefetch?: boolean;
   children?: ReactNode;
-};
+} & ComponentPropsWithoutRef<'a'>;
+
+export type LinkType = ComponentType<LinkProps>;
 
 export const Link = forwardRef(function Link(props: LinkProps, ref: Ref<HTMLAnchorElement>) {
-  const { href, replace, scroll, prefetch, children } = props;
+  const { href, replace, scroll, prefetch, children, ...attributes } = props;
   return (
-    <NextLink href={href} replace={replace} scroll={scroll} prefetch={prefetch} ref={ref}>
+    <NextLink href={href} replace={replace} scroll={scroll} prefetch={prefetch} ref={ref} {...attributes}>
       {children}
     </NextLink>
   );
